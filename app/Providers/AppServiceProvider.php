7c3d190213eaddbeeb71d693207dd4d332b4be25
za0888 <56291618+za0888@users.noreply.github.com>
2023-12-18 23:14:08 +0200

    plus livewire

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Providers;
+
+use App\enums\UserStatus;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        Blade::if('noCustomer', function () {
+            $userStatus = auth()->user()->status->value;
+
+            return $userStatus !== UserStatus::Customer->value;
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+
+        Model::preventLazyLoading(!$this->app->isProduction());
+
+        Model::shouldBeStrict(!$this->app->isProduction());
+
+//        @bloger
+        Blade::if('manager',
+            fn() => Auth::user()?->status === UserStatus::Manager);
+
+        Gate::before(fn(User $user) => $user->status === UserStatus::Administrator);
+    }
+
+}
